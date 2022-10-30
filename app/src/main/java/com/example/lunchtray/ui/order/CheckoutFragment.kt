0189bcca709a793fdcd8f8060ab462a04aa7ff68
Magus Dev <114507764+theMagusDev@users.noreply.github.com>
2022-10-30 16:33:00 +0300

    Implement submitOrder() function

--- app/src/main/java/com/example/lunchtray/ui/order/CheckoutFragment.kt
+++ app/src/main/java/com/example/lunchtray/ui/order/CheckoutFragment.kt
@@ -82,8 +82,8 @@ class CheckoutFragment : Fragment() {
     fun submitOrder() {
         // Show snackbar to "confirm" order
         Snackbar.make(binding.root, R.string.submit_order, Snackbar.LENGTH_SHORT).show()
-        // TODO: Reset order in view model
-        // TODO: Navigate back to the [StartFragment] to start over
+        sharedViewModel.resetOrder()
+        findNavController().navigate(R.id.action_checkoutFragment_to_startOrderFragment)
     }
 
     /**
